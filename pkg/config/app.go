41837fb79a8fb0bda01704ad38baf175e298234f
Qayyuum Harun <qayyuum.harun@outlook.com>
2023-06-11 22:26:22 +0200

    Use docker compose to run locally

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +16,9 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "username:password/tableName?charset=utf8&parseTime=True&loc=Local")
+	config := getDBConfig()
+
+	d, err := gorm.Open("mysql", config.generateConnectionString())
 	if err != nil {
 		panic(err)
 	}
@@ -20,4 +28,38 @@ func Connect() {
 
 func GetDB() *gorm.DB {
 	return db
+}
+
+func getEnv(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+type DBConfig struct {
+	Username string
+	Password string
+	Database string
+}
+
+func getDBConfig() DBConfig {
+	username := getEnv("MYSQL_USERNAME")
+	password := getEnv("MYSQL_PASSWORD")
+	database := getEnv("MYSQL_DATABASE")
+
+	return DBConfig{
+		Username: username,
+		Password: password,
+		Database: database,
+	}
+}
+
+func (c *DBConfig) generateConnectionString() string {
+	config := getDBConfig()
+
+	return fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8&parseTime=True&loc=Local", config.Username, config.Password, config.Database)
 }
